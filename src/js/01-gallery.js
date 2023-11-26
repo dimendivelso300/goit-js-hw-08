2b7c6e85697b425742c24b269742ab9e50606aff
dimendivelso300 <84195227+dimendivelso300@users.noreply.github.com>
2023-11-26 02:25:49 -0500

    homework8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function creargallery(items) {
+    return items
+    .map((item) => `<div class="gallery__item">
+                    <a class="gallery__link" href="${item.original}">
+                    <img class="gallery__image"
+                    src="${item.preview}"
+                    data-sourse="${item.original}"
+                    alt="${item.description}"/>
+                    </a>
+                    </div>`).join("");
+
+}
+
+const agregarImagen = creargallery(galleryItems);
+
+gallery.innerHTML = agregarImagen;
+
+function action(imageAction) {
+    imageAction.preventDefault();
+}
+
+let lightbox = new SimpleLightbox ('.gallery a', {captionsData: 'alt', captionDelay: 250});
